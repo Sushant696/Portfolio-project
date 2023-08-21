6fc97287d94fb527f028c52b95aa0e3f01f8b9c6
sushantPrasai <sushantprasaii7@gmail.com>
2023-08-21 20:38:32 +0545

    final commit

--- src/main.tsx
+++ src/main.tsx
@@ -11,4 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-// use route and routes path.tsx will do all the job make an object inside which you will use routes and route element and everything will be handled there and regarding the routing the navbar is also involved somewhat in the routing project 
--- src/pages/about/styling.ts
+++ src/pages/about/styling.ts
@@ -8,40 +8,34 @@ export const HomePageWrapper = styled("div")`
 export const HomeWrapper = styled("div")`
   display: flex;
   align-items: center;
-  @media(max-width : 1236px) {
-    display:block;
+  @media (max-width: 1236px) {
+    display: block;
   }
 `;
 
 export const SushantImg = styled("div")`
-flex: 1;
-padding:3rem;
+  flex: 1;
+  padding: 3rem;
 `;
 export const StyledImg = styled("img")`
   max-width: 95%;
   border-radius: 18px;
 `;
 
-
 export const StyledContent = styled("div")`
   flex: 1;
 `;
 export const StyledHeading = styled("h1")`
-  font-size:56px;
+  font-size: 56px;
 
-@media(max-width : 1400px) {
-  font-size:36px;
-}
+  @media (max-width: 1400px) {
+    font-size: 36px;
+  }
 `;
 export const StyledSubHeading = styled("h1")`
-  font-size:26px;
+  font-size: 26px;
 
-@media(max-width : 1400px) {
-  font-size:24px;
-}
-
-
+  @media (max-width: 1400px) {
+    font-size: 24px;
+  }
 `;
-
-
-// media for image and content + heading font media 
