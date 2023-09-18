9b704b3fffb4aea4d2b9a2da5f6f6d9f99650173
sushant babu prasai <sushantprasaii7@gmail.com>
2023-09-18 13:36:09 +0545

    Progress bar

--- src/pages/homepage/styling.ts
+++ src/pages/homepage/styling.ts
@@ -37,8 +37,6 @@ export const StyledCard = styled("div")`
   color: #111;
   max-width: 180px;
   border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-
   &:hover {
     
   }
--- src/pages/homepage/tech.tsx
+++ src/pages/homepage/tech.tsx
@@ -4,38 +4,39 @@ import css from "../../assets/Images/css.png";
 import github from "../../assets/Images/github.png";
 import js from "../../assets/Images/js.png";
 import react from "../../assets/Images/react.png";
+import { Progress } from "antd";
+
 export default function Tech() {
   return (
     <Wrapper className=" mx-1">
-      <h1 className="text-center text-[36px] text-[#000] font-semibold">
+      <h1 className="text-center text-[36px] my-2 py-2 text-[#000] font-semibold">
         My Profeciency
       </h1>
-      <div className="m-2 p-2  flex justify-around gap-[]">
+      <div className="m-2 p-2  flex justify-center gap-[4rem]">
         <StyledCard>
           <img alt="Html" className=" m-2 w-[70%]" src={`${Html}`} />
           <h1 className="text-center">Html</h1>
-          <h2>Progress bar </h2>
+          <Progress strokeColor={"#ff8d3f"} percent={70} status="active" />
         </StyledCard>
         <StyledCard>
           <img alt="Html" className="m-2 w-[70%]" src={`${css}`} />
           <h1>Css</h1>
-          <h2>Progress bar </h2>
+          <Progress percent={60} status="active" strokeColor={"#ff8d3f"} />
         </StyledCard>
         <StyledCard>
           <img alt="Html" className="m-2 w-[70%]" src={`${js}`} />
           <h1>JavaScript</h1>
-          <h2>Progress bar </h2>
+          <Progress percent={80} strokeColor={"#ff8d3f"} status="active" />
         </StyledCard>
         <StyledCard>
           <img alt="Html" className="m-2 bg-[#111] w-[70%]" src={`${github}`} />
           <h1>GitHub </h1>
-          <h2>Progress bar </h2>
+          <Progress strokeColor={"#ff8d3f"} percent={70} status="active" />
         </StyledCard>
         <StyledCard>
-          <img alt="Html" className="m-2 w-[70%]" src={`${react}`} />
+          <img alt="Html"  className="m-2 w-[70%]" src={`${react}`} />
           <h1>React</h1>
-          <h2>Progress bar </h2>
-
+          <Progress  strokeColor={"#ff8d3f"} percent={40} status="active" />
         </StyledCard>
       </div>
     </Wrapper>
