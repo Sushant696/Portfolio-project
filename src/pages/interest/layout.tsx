231fd7da611d01823e3894b9389d69ccbb393eff
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-12 16:41:36 +0545

    interest routing

---
+++ src/pages/interest/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer/footerLinks";
+import ShowCase from "../homepage/showcase";
+import Interest from "./index.";
+
+export default function Layout() {
+  return (
+    <div>
+      <ShowCase/>
+      <Interest/>
+      <Footer/>
+    </div>
+  )
+}
--- src/routing/path.tsx
+++ src/routing/path.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import Interest from "../pages/interest/index.";
 import Skills from "../pages/skills";
 import Homepage from "../pages/homepage";
 import Aboutlayout from "../pages/about/layout";
 import ContactLayout from "../pages/contact/layout";
+import Interestlayout from "../pages/interest/layout";
 export default function Routing() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Aboutlayout />} />
-        <Route path="/interest" element={<Interest />} />
+        <Route path="/interest" element={<Interestlayout/>} />
         <Route path="/contact" element={<ContactLayout />} />
         <Route path="/skills" element={<Skills />} />
       </Routes>
