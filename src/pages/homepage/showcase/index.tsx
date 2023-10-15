f8029282a8988f86e17a4ebde6099f95e3ad0911
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-15 15:31:13 +0545

    updates on showcase

---
+++ src/pages/homepage/showcase/index.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { StyledShowCase, StyledHeading, StyledImg } from "./styling";
+// export default function ShowCase() {
+//   return (
+//     <>
+//       <StyledShowCase className="flex justify-center items-center mb-[2rem]">
+//         <div>
+//           <StyledHeading>
+//             <h1 className="text-[64px] text-[#DDD] font-bold">Sushant Babu Prasai</h1>
+//           </StyledHeading>
+//           <br />
+//           <h1 className="font-semibold text-[#DDD] text-[36px] ">Student | Programmer | Learner | Dreamer</h1>
+//         </div>
+//         <StyledImg>{/* image goes here */}</StyledImg>
+//       </StyledShowCase>
+//     </>
+//   );
+// }
